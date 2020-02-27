d8d22ba5c2a46e30b11388c745213303dd6148a2
teunbrand <tahvdbrand@gmail.com>
2020-02-27 22:28:24 +0100

    Add S4 Cartesian Coordinates

---
+++ R/coord_constructors.R
@@ -0,0 +1,24 @@
+#' @title Cartesian coordinates for S4 classes
+#'
+#' @description The Cartesian coordinate system is the typical linear coordinate
+#'   system wherein a point on the plane is parameterised by the x- and
+#'   y-position and distances between point are Euclidean. The S4 variant allows
+#'   the contextual evaluation of, for example, ranged data classes.
+#'
+#' @inheritParams ggplot2::coord_cartesian
+#'
+#' @details This coordinate function is designed to work with S4 scales. In
+#'   absence thereof, no contextual evaluation should occur.
+#'
+#'   As a temporary workaround, when the coordinates are set up, all S4 Vector
+#'   classes are disguised as the \code{WoodenHorse} class.
+#'
+#' @export
+#'
+#' @examples
+#' NULL
+coord_S4 <- function(xlim = NULL, ylim = NULL, expand = TRUE, default = FALSE,
+                     clip = "on") {
+  ggproto(NULL, CoordS4, limits = list(x = xlim, y = ylim),
+          expand = expand, default = default, clip = clip)
+}

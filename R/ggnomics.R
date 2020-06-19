b0a95a0fbc2a0242b12e0d7423121d7cd560bacb
teunbrand <tahvdbrand@gmail.com>
2020-06-19 21:06:58 +0200

    Purge master

--- R/ggnomics.R
+++
@@ -1,9 +0,0 @@
-#' @importFrom methods as getMethod
-#' @importFrom stats setNames rt dcauchy
-#' @importFrom utils head tail getFromNamespace
-#' @import ggplot2
-#' @import scales
-#' @import grid
-#' @import gtable
-#' @import data.table
-NULL
--- tests/testthat.R
+++
@@ -1,5 +0,0 @@
-library(testthat)
-library(ggnomics)
-library(ggplot2)
-
-test_check("ggnomics")

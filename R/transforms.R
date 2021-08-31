1e98178ac8c2c2fe8531c4fd6f0886116d1e0c94
teunbrand <tahvdbrand@gmail.com>
2021-08-31 16:18:10 +0200

    Clean slate

--- R/transforms.R
+++
@@ -1,9 +0,0 @@
-# Currently only identity transformation
-S4TransIdentity <- scales::trans_new(
-    "S4identity",
-    "force",
-    "force",
-    breaks = S4BreaksMajor,
-    minor_breaks = S4BreaksMinor,
-    format = S4LabelFormat
-)
